265b559f499b2038e7252879109082b4b1100953
Rijvi Rajib <me@rij.co>
2016-04-06 10:47:41 -0700

    Install missing package dependencies

--- src/components/AppointmentFormDateTimePicker.js
+++ src/components/AppointmentFormDateTimePicker.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import moment from 'moment';
-import _ from 'underscore';
+import _ from 'lodash';
 import classnames from 'classnames';
 import AppointmentFormTimePicker from './AppointmentFormTimePicker';
 
